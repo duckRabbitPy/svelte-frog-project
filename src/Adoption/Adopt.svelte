8e06e67f72e373628cef19dcd309e922ecdb0e71
hallobros <67079621+hallobros@users.noreply.github.com>
2021-03-18 01:02:19 +0000

    added firebase authentication

--- src/Adoption/Adopt.svelte
+++ src/Adoption/Adopt.svelte
@@ -4,6 +4,16 @@ import { createEventDispatcher } from "svelte";
 import CustomButton from "../UI/CustomButton.svelte";
 import Modal from "../UI/Modal.svelte";
 
+//added to .gitIgnore
+import { firebaseConfig } from "./firebaseConfig.js"
+
+
+import firebase from 'firebase/app';
+import Auth from '../Auth.svelte';
+
+firebase.initializeApp(firebaseConfig);
+
+
 const dispatch = createEventDispatcher();
 
 function cancel() {
@@ -20,23 +30,48 @@ function cancel() {
     p {
         color: red;
     }
+
+    h2 {
+        color: green;
+    }
 </style>
 
     <Modal title="Adopting a frog" on:cancel>
-    <form in:fade>
     <div>
-        <label for="Username">Username</label>
-        <input type="text">
+        <div class="wrapper">
+            <Auth
+              useRedirect={true}
+              let:user
+              let:loggedIn
+              let:loginWithGoogle
+              let:logout>
+              
+            {#if !loggedIn}
+            <p>You must sign in/register to adopt a frog</p>
+             {/if}
+              {#if loggedIn}
+                <div>
+                  <div>
+                    <h2>Logged in as {user.email}</h2>
+                    <button type="button" class="mt-3" on:click={logout}>Logout</button>
+                  </div>
+                </div>
+              {:else}
+                <div class="w-full max-w-xs">
+                  <form on:submit|preventDefault>
+                    <div class="mt-3">
+                      <button type="button" on:click|preventDefault={loginWithGoogle}>
+                        Sign In with Google
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              {/if}
+            </Auth>
+          </div>
     </div>
-    <div>
-        <label for="Password">Password</label>
-        <input type="text">
-    </div>
-        <p>You must sign in/register to adopt a frog</p>
-        <CustomButton btntype="submit" >Sign in</CustomButton>
-    </form>
     <div slot="footer">
-        <CustomButton btntype="submit" >Register</CustomButton>
+        <CustomButton btntype="submit" >Next steps!</CustomButton>
         <CustomButton btntype="button" on:click="{cancel}">Cancel</CustomButton>
     </div>
     </Modal>
---
+++ src/Adoption/firebaseConfig.js
@@ -0,0 +1,11 @@
+//gitIgnore to hide credentials 
+export let firebaseConfig = {
+    apiKey: "AIzaSyBQVbwdP2FX4oybK9DeJahVSdB8pvXJ2GU",
+    authDomain: "svelte-firebase-bknd.firebaseapp.com",
+    databaseURL: "https://svelte-firebase-bknd-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "svelte-firebase-bknd",
+    storageBucket: "svelte-firebase-bknd.appspot.com",
+    messagingSenderId: "1054394742369",
+    appId: "1:1054394742369:web:bf162d06c4166290f89c20",
+    measurementId: "G-8NFPZLWW48"
+  };
