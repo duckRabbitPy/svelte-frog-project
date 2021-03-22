ba7d6d250a2a32ea5b500b8825a8a09ac4c68048
duckRabbitPy <duck.rabbit.python@gmail.com>
2021-03-22 09:27:58 +0000

    added static image files to products

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -11202,7 +11202,7 @@ var app = (function () {
         title: 'Frog T-shirt',
         price: 25,
         description: 'Available in all sizes',
-        srcVar: 'https://www.thebearhug.com/images/the-bearhug-co-ltd-laser-frog-t-shirt-p958-8377_medium.jpg',
+        srcVar: '/images/tshirt.jpeg',
         discount: null,
       },
       {
@@ -11210,7 +11210,7 @@ var app = (function () {
         title: 'Frog Mug',
         price: 6,
         description: "Sweeeeeeeeet frog mug. BUY IT",
-        srcVar: 'https://img.ltwebstatic.com/images3_pi/2020/12/03/160697495119c005d9d00195e053fdf06c4c7ae50d_thumbnail_900x.webp',
+        srcVar: 'images/mugs.webp',
         discount: null,
       },
       {
@@ -11218,7 +11218,7 @@ var app = (function () {
         title: 'Frog tea lights',
         price: 4.5,
         description: "Cosy and fragrant",
-        srcVar: 'https://i.etsystatic.com/12941269/r/il/a9c128/998648306/il_1588xN.998648306_13wj.jpg',
+        srcVar: '/images/candle.jpg',
         discount: 'SAVE 20%'
       },
       {
@@ -11226,7 +11226,7 @@ var app = (function () {
         title: 'Frog music album',
         price: 8,
         description: "Frog lyrics and slap bass tunes",
-        srcVar: 'http://www.progarchives.com/progressive_rock_discography_covers/10980/cover_3211152782019_r.jpg',
+        srcVar: '/images/album.jpeg',
         discount: null,
       },
       {
@@ -11234,7 +11234,7 @@ var app = (function () {
         title: 'FrogStation 7',
         price: 248,
         description: "Modern gaming for modern times",
-        srcVar: 'https://www.decalgirl.com/assets/items/xboc/800/xboc-frog.5.jpg',
+        srcVar: '/images/frogstation.jpg',
         discount: null,
       },
 
--- src/Products/products-store.js
+++ src/Products/products-store.js
@@ -6,7 +6,7 @@ export const products = writable([
     title: 'Frog T-shirt',
     price: 25,
     description: 'Available in all sizes',
-    srcVar: 'https://www.thebearhug.com/images/the-bearhug-co-ltd-laser-frog-t-shirt-p958-8377_medium.jpg',
+    srcVar: '/images/tshirt.jpeg',
     discount: null,
   },
   {
@@ -14,7 +14,7 @@ export const products = writable([
     title: 'Frog Mug',
     price: 6,
     description: "Sweeeeeeeeet frog mug. BUY IT",
-    srcVar: 'https://img.ltwebstatic.com/images3_pi/2020/12/03/160697495119c005d9d00195e053fdf06c4c7ae50d_thumbnail_900x.webp',
+    srcVar: 'images/mugs.webp',
     discount: null,
   },
   {
@@ -22,7 +22,7 @@ export const products = writable([
     title: 'Frog tea lights',
     price: 4.5,
     description: "Cosy and fragrant",
-    srcVar: 'https://i.etsystatic.com/12941269/r/il/a9c128/998648306/il_1588xN.998648306_13wj.jpg',
+    srcVar: '/images/candle.jpg',
     discount: 'SAVE 20%'
   },
   {
@@ -30,7 +30,7 @@ export const products = writable([
     title: 'Frog music album',
     price: 8,
     description: "Frog lyrics and slap bass tunes",
-    srcVar: 'http://www.progarchives.com/progressive_rock_discography_covers/10980/cover_3211152782019_r.jpg',
+    srcVar: '/images/album.jpeg',
     discount: null,
   },
   {
@@ -38,7 +38,7 @@ export const products = writable([
     title: 'FrogStation 7',
     price: 248,
     description: "Modern gaming for modern times",
-    srcVar: 'https://www.decalgirl.com/assets/items/xboc/800/xboc-frog.5.jpg',
+    srcVar: '/images/frogstation.jpg',
     discount: null,
   },
 
